ba196ffd2bb6bff54510c5bc6bdd563a80953153
Anastasiia Tokareva <wwwnasty11@rambler.ru>
2024-07-10 17:30:24 +0300

    .gitignore

---
+++ src/test/java/StatsServiceTest.java
@@ -0,0 +1,72 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+
+    @Test
+    public void sumSalesTest() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 180;
+        long actual = service.sumSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void avgSalesTest() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 15;
+        long actual = service.avgSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxSalesTest() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 8;
+        long actual = service.maxSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void minSalesTest() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 9;
+        long actual = service.minSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void LessThanAvgSalesTest() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 5;
+        long actual = service.lessThanAvgSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void HigherThanAvgSalesTest() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 5;
+        long actual = service.higherThanAvgSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
